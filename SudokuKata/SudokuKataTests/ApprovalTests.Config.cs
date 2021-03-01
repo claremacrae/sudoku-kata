bfb8eef2164cda6764ae47d6fac980e8d37016df
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-01 08:22:17 +0000

    t Add ApprovalTests.Config.cs - set default reporter, and put approved files in approval_tests sub-directory

---
+++ SudokuKata/SudokuKataTests/ApprovalTests.Config.cs
@@ -0,0 +1,6 @@
+﻿using ApprovalTests.Reporters;
+using ApprovalTests.Namers;
+
+[assembly: UseReporter(typeof(AraxisMergeReporter))]
+// [assembly: UseReporter(typeof(AllFailingTestsClipboardReporter))]
+[assembly: UseApprovalSubdirectory("approval_tests")]
