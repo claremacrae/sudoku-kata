8dc1ceffacb18587751f30db7ab0713c6c161e1f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-01 08:25:49 +0000

    t Move ConsoleOutput out to separate file

--- SudokuKata/SudokuKataTests/ProgramTests.cs
+++ SudokuKata/SudokuKataTests/ProgramTests.cs
@@ -1,41 +1,13 @@
 using ApprovalTests;
 using SudokuKata;
 using System;
-using System.IO;
 using SudokuKata.Utililities;
 using Xunit;
 
 namespace SudokuKataTests
 {
-    public class ProgramTests
+    public partial class ProgramTests
     {
-
-        // From http://www.vtrifonov.com/2012/11/getting-console-output-within-unit-test.html
-        public class ConsoleOutput : IDisposable
-        {
-            private StringWriter stringWriter;
-            private TextWriter originalOutput;
-
-            public ConsoleOutput()
-            {
-                stringWriter = new StringWriter();
-                originalOutput = Console.Out;
-                Console.SetOut(stringWriter);
-            }
-
-            public string GetOuput()
-            {
-                return stringWriter.ToString();
-            }
-
-            public void Dispose()
-            {
-                Console.SetOut(originalOutput);
-                stringWriter.Dispose();
-            }
-        }
-
- 
         [Fact]
         public void Test1()
         {
