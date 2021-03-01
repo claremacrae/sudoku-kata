f5ab99f4fbdea8d3af1c9ae092dfa4afc9063a78
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-01 07:19:57 +0000

    r Extract RandomNumber to separate file

--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -5,34 +5,6 @@ using System.Text;
 
 namespace SudokuKata
 {
-    public class RandomNumber : IRandomValueGenerator
-    {
-        private Random rnd;
-        public RandomNumber()
-        {
-            rnd = new Random();
-        }
-
-        public RandomNumber(Int32 seed)
-        {
-            rnd = new Random(seed);
-        }
-
-        public int Next()
-        {
-            var next = rnd.Next();
-            Console.WriteLine(">>> Next random number: {0}.", next);
-            return next;
-        }
-
-        public int Next(int limit)
-        {
-            var next = rnd.Next(limit);
-            Console.WriteLine(">>> Next random number: {0} out of {1}.", next, limit);
-            return next;
-        }
-    }
-
     public class Program
     {
         public static void Play(IRandomValueGenerator rng)
---
+++ SudokuKata/SudokuKata/RandomNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SudokuKata
+{
+    public class RandomNumber : IRandomValueGenerator
+    {
+        private Random rnd;
+        public RandomNumber()
+        {
+            rnd = new Random();
+        }
+
+        public RandomNumber(Int32 seed)
+        {
+            rnd = new Random(seed);
+        }
+
+        public int Next()
+        {
+            var next = rnd.Next();
+            Console.WriteLine(">>> Next random number: {0}.", next);
+            return next;
+        }
+
+        public int Next(int limit)
+        {
+            var next = rnd.Next(limit);
+            Console.WriteLine(">>> Next random number: {0} out of {1}.", next, limit);
+            return next;
+        }
+    }
+}
