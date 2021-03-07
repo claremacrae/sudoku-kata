1c9a9b5dbd046422517e697f80a630dc873ecb78
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-07 11:32:49 +0000

    F Copy in GridStringifier and tests from refactor3 branch

---
+++ SudokuKata/SudokuKata/Utilities/GridStringifier.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace SudokuKata.Utilities
+{
+    public class GridStringifier
+    {
+        public static string ConvertToString(int[] board)
+        {
+            const string divider = "+---+---+---+\n";
+            var result = divider;
+            result += ToRowNumbered(board, 0);
+            result += ToRowNumbered(board, 1);
+            result += ToRowNumbered(board, 2);
+            result += divider;
+            result += ToRowNumbered(board, 3);
+            result += ToRowNumbered(board, 4);
+            result += ToRowNumbered(board, 5);
+            result += divider;
+            result += ToRowNumbered(board, 6);
+            result += ToRowNumbered(board, 7);
+            result += ToRowNumbered(board, 8);
+            result += divider;
+            return result;
+        }
+
+        private static string ToRowNumbered(int[] board, int row)
+        {
+            var o = row * 9;
+            return ToRow(board, o + 0, o + 1, o + 2, o + 3, o + 4, o + 5, o + 6, o + 7, o + 8);;
+        }
+
+        private static string ToRow(int[] board, int index0, int index1, int index2, int index3, int index4, int index5,
+            int index6, int index7, int index8)
+        {
+            var row = "|";
+            row += ToTriplet(board, index0, index1, index2);
+            row += "|";
+            row += ToTriplet(board, index3, index4, index5);
+            row += "|";
+            row += ToTriplet(board, index6, index7, index8);
+            row += "|";
+            row += "\n";
+            return row;
+        }
+
+        private static string ToTriplet(int[] board, int index0, int index1, int index2)
+        {
+            var value1 = board[index0];
+            var value2 = board[index1];
+            var value3 = board[index2];
+            return ToCell(value1) + ToCell(value2) + ToCell(value3);
+        }
+
+        private static string ToCell(int value)
+        {
+            return value == 0 ? "." : Convert.ToString(value);
+        }
+
+        public static string ConvertToCode(int[] board)
+        {
+            return string.Join(string.Empty, board);
+        }
+    }
+}
---
+++ SudokuKata/SudokuKataTests/GridStringifierTests.cs
@@ -0,0 +1,73 @@
+﻿using ApprovalTests;
+using SudokuKata;
+using SudokuKata.Utilities;
+using Xunit;
+
+namespace SudokuKataTests
+{
+    public class GridStringifierTests
+    {
+        [Fact]
+        public void TestPopulatedBoard()
+        {
+            // Assert
+            int[] input =
+            {
+                4, 2, 9, 5, 3, 8, 6, 1, 7, 6, 3, 7, 9, 1, 4, 8, 2, 5, 8, 5, 1, 7, 6, 2, 4, 3, 9, 9, 7, 5, 6, 8, 3, 1, 4,
+                2, 1, 6, 2, 4, 5, 7, 9, 8, 3, 3, 4, 8, 1, 2, 9, 5, 7, 6, 2, 1, 6, 8, 7, 5, 3, 9, 4, 5, 9, 3, 2, 4, 1, 7,
+                6, 8, 7, 8, 4, 3, 9, 6, 2, 5, 1
+            };
+            VerifyBoardAsString(input);
+        }
+
+        [Fact]
+        public void TestPopulatedBoardAsCode()
+        {
+            // TODO Are there sections, where I could use the input twice, for two separate outputs?
+            // Assert
+            int[] input =
+            {
+                4, 2, 9, 5, 3, 8, 6, 1, 7, 6, 3, 7, 9, 1, 4, 8, 2, 5, 8, 5, 1, 7, 6, 2, 4, 3, 9, 9, 7, 5, 6, 8, 3, 1, 4,
+                2, 1, 6, 2, 4, 5, 7, 9, 8, 3, 3, 4, 8, 1, 2, 9, 5, 7, 6, 2, 1, 6, 8, 7, 5, 3, 9, 4, 5, 9, 3, 2, 4, 1, 7,
+                6, 8, 7, 8, 4, 3, 9, 6, 2, 5, 1
+            };
+            VerifyBoardAsCode(input);
+        }
+
+        [Fact]
+        public void TestPartialBoard()
+        {
+            // Assert
+            int[] input =
+            {
+                0, 0, 0, 5, 0, 8, 0, 0, 7, 6, 0, 7, 0, 1, 0, 0, 2, 0, 0, 5, 0, 0, 0, 0, 0, 3, 9, 0, 7, 0, 6, 8, 0, 1, 0,
+                0, 0, 6, 0, 0, 5, 7, 0, 0, 3, 0, 0, 8, 1, 0, 0, 0, 7, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 5, 9, 0, 0, 4, 1, 7,
+                0, 0, 0, 0, 0, 3, 0, 0, 0, 5, 1
+            };
+            VerifyBoardAsString(input);
+        }
+        
+        [Fact]
+        public void TestPartialBoardAsCode()
+        {
+            // Assert
+            int[] input =
+            {
+                0, 0, 0, 5, 0, 8, 0, 0, 7, 6, 0, 7, 0, 1, 0, 0, 2, 0, 0, 5, 0, 0, 0, 0, 0, 3, 9, 0, 7, 0, 6, 8, 0, 1, 0,
+                0, 0, 6, 0, 0, 5, 7, 0, 0, 3, 0, 0, 8, 1, 0, 0, 0, 7, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 5, 9, 0, 0, 4, 1, 7,
+                0, 0, 0, 0, 0, 3, 0, 0, 0, 5, 1
+            };
+            VerifyBoardAsCode(input);
+        }
+
+        private static void VerifyBoardAsString(int[] input)
+        {
+            Approvals.Verify(GridStringifier.ConvertToString(input));
+        }
+
+        private static void VerifyBoardAsCode(int[] input)
+        {
+            Approvals.Verify(GridStringifier.ConvertToCode(input));
+        }
+    }
+}
