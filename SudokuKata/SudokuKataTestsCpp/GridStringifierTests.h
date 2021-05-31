f18d88fee59807646309ee9cacf9858008cd8aca
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-05-31 21:52:00 +0100

    t Implement Catch2 tests for GridStringifierTests

--- SudokuKata/SudokuKataTestsCpp/GridStringifierTests.h
+++ SudokuKata/SudokuKataTestsCpp/GridStringifierTests.h
@@ -2,33 +2,11 @@
 
 #include <vector>
 
-using namespace ApprovalTests;
-using namespace SudokuKata;
-using namespace SudokuKata::Utilities;
-using namespace Xunit;
-
 namespace SudokuKataTests
 {
 	class GridStringifierTests
 	{
-	public:
-//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
-//ORIGINAL LINE: [Fact] public void TestPopulatedBoard()
-		void TestPopulatedBoard();
-
-//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
-//ORIGINAL LINE: [Fact] public void TestPopulatedBoardAsCode()
-		void TestPopulatedBoardAsCode();
-
-//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
-//ORIGINAL LINE: [Fact] public void TestPartialBoard()
-		void TestPartialBoard();
-
-//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
-//ORIGINAL LINE: [Fact] public void TestPartialBoardAsCode()
-		void TestPartialBoardAsCode();
-
-	private:
+    public:
 		static void VerifyBoardAsString(std::vector<int> &input);
 
 		static void VerifyBoardAsCode(std::vector<int> &input);
