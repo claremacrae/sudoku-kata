159455e7a2c35c01fbc1cdc849d537fd5c0cfb26
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-05-31 10:38:17 +0100

    R Remove some repetition of code

--- SudokuKata/SudokuKata/Utilities/FileWritingRandomNumber.cs
+++ SudokuKata/SudokuKata/Utilities/FileWritingRandomNumber.cs
@@ -6,19 +6,18 @@ namespace SudokuKata
 {
     public class FileWritingRandomNumber : IRandomValueGenerator
     {
-        private Random rnd;
+        private RandomNumber rnd;
         List<int> _results = new List<int>();
 
         public FileWritingRandomNumber(Int32 seed)
         {
-            rnd = new Random(seed);
+            rnd = new RandomNumber(seed);
         }
 
         public int Next()
         {
             var next = rnd.Next();
             _results.Add(next);
-            // Console.WriteLine(">>> Next random number: {0}.", next);
             return next;
         }
 
@@ -26,13 +25,11 @@ namespace SudokuKata
         {
             var next = rnd.Next(limit);
             _results.Add(next);
-            // Console.WriteLine(">>> Next random number: {0} out of {1}.", next, limit);
             return next;
         }
 
         public void WriteValuesToFile(string seedsFile)
         {
-            // System.IO.File.WriteAllLines(seedsFile, _results);
             using(TextWriter tw = new StreamWriter(seedsFile))
             {
                 foreach (int s in _results)
