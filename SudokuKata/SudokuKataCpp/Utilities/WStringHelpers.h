2fede0885d35ab577305afaa683d0cdea30dfbd9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-06-01 09:35:11 +0100

    R Split implementation of WStringHelpers.h to .cpp

--- SudokuKata/SudokuKataCpp/Utilities/WStringHelpers.h
+++ SudokuKata/SudokuKataCpp/Utilities/WStringHelpers.h
@@ -3,18 +3,8 @@
 #include <codecvt>
 #include <locale>
 
-std::string toString(const std::wstring &wstr) {
-    static std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> utf8_converter;
-    return utf8_converter.to_bytes(wstr);
-}
+std::string toString(const std::wstring &wstr);
 
-std::string toString(const wchar_t *wstr) {
-    static std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> utf8_converter;
-    return utf8_converter.to_bytes(wstr);
-}
+std::string toString(const wchar_t *wstr);
 
-std::wstring toWString(const std::string &str) {
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    std::wstring wide = converter.from_bytes(str);
-    return wide;
-}
+std::wstring toWString(const std::string &str);
