bfdd2bc6d7a491bbc481e83646cd7750c27e3655
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-06-03 12:10:58 +0100

    F Add C# class TwoDigitMaskGroups.cs - for ease of debug printing

---
+++ SudokuKata/SudokuKata/Utilities/TwoDigitMaskGroups.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+
+namespace SudokuKata
+{
+    public partial class Program
+    {
+        public class TwoDigitMaskGroups
+        {
+            public int Mask { get; }
+            public int Discriminator { get; }
+            public string Description { get; }
+            public IGrouping<int, CellGroups> Cells { get; }
+
+            public TwoDigitMaskGroups(int mask, int discriminator, string description, IGrouping<int, CellGroups> cells)
+            {
+                Mask = mask;
+                Discriminator = discriminator;
+                Description = description;
+                Cells = cells;
+            }
+
+            public override string ToString()
+            {
+                var result = $"{nameof(Mask)}: {Mask}, {nameof(Discriminator)}: {Discriminator}, {nameof(Description)}: {Description}\n";
+                foreach (var cellGroups in Cells)
+                {
+                    result += $"   {cellGroups.ToString()}\n";
+                }
+
+                return result;
+            }
+        }
+    }
+}
