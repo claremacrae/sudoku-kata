d6b41fd5fc34f9f59e3081f5329233c424bef1ff
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-06-03 12:09:45 +0100

    F Add C# class CellGroups.cs - for ease of debug printing

---
+++ SudokuKata/SudokuKata/Utilities/CellGroups.cs
@@ -0,0 +1,29 @@
+namespace SudokuKata
+{
+    public partial class Program
+    {
+        public class CellGroups
+        {
+            public int Discriminator { get; }
+            public string Description { get; }
+            public int Index { get; }
+            public int Row { get; }
+            public int Column { get; }
+
+            public CellGroups(int discriminator, string description, int index, int row, int column)
+            {
+                Discriminator = discriminator;
+                Description = description;
+                Index = index;
+                Row = row;
+                Column = column;
+            }
+
+            public override string ToString()
+            {
+                return
+                    $"{nameof(Discriminator)}: {Discriminator}, {nameof(Description)}: {Description}, {nameof(Index)}: {Index}, {nameof(Row)}: {Row}, {nameof(Column)}: {Column}";
+            }
+        }
+    }
+}
