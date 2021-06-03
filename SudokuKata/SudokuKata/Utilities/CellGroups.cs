6f738718d6012b146c7cf26b5ae6390776d0cc45
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-06-03 12:13:48 +0100

    r Move CellGroups to parent scope

--- SudokuKata/SudokuKata/Utilities/CellGroups.cs
+++ SudokuKata/SudokuKata/Utilities/CellGroups.cs
@@ -1,29 +1,26 @@
 namespace SudokuKata
 {
-    public partial class Program
+    public class CellGroups
     {
-        public class CellGroups
+        public int Discriminator { get; }
+        public string Description { get; }
+        public int Index { get; }
+        public int Row { get; }
+        public int Column { get; }
+
+        public CellGroups(int discriminator, string description, int index, int row, int column)
         {
-            public int Discriminator { get; }
-            public string Description { get; }
-            public int Index { get; }
-            public int Row { get; }
-            public int Column { get; }
+            Discriminator = discriminator;
+            Description = description;
+            Index = index;
+            Row = row;
+            Column = column;
+        }
 
-            public CellGroups(int discriminator, string description, int index, int row, int column)
-            {
-                Discriminator = discriminator;
-                Description = description;
-                Index = index;
-                Row = row;
-                Column = column;
-            }
-
-            public override string ToString()
-            {
-                return
-                    $"{nameof(Discriminator)}: {Discriminator}, {nameof(Description)}: {Description}, {nameof(Index)}: {Index}, {nameof(Row)}: {Row}, {nameof(Column)}: {Column}";
-            }
+        public override string ToString()
+        {
+            return
+                $"{nameof(Discriminator)}: {Discriminator}, {nameof(Description)}: {Description}, {nameof(Index)}: {Index}, {nameof(Row)}: {Row}, {nameof(Column)}: {Column}";
         }
     }
 }
