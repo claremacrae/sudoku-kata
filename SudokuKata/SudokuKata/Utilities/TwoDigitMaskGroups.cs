b9f15419bb06e08d26d166562f32e443b89e63d1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-06-03 12:14:22 +0100

    r Move TwoDigitMaskGroups to parent scope

--- SudokuKata/SudokuKata/Utilities/TwoDigitMaskGroups.cs
+++ SudokuKata/SudokuKata/Utilities/TwoDigitMaskGroups.cs
@@ -2,33 +2,30 @@ using System.Linq;
 
 namespace SudokuKata
 {
-    public partial class Program
+    public class TwoDigitMaskGroups
     {
-        public class TwoDigitMaskGroups
+        public int Mask { get; }
+        public int Discriminator { get; }
+        public string Description { get; }
+        public IGrouping<int, CellGroups> Cells { get; }
+
+        public TwoDigitMaskGroups(int mask, int discriminator, string description, IGrouping<int, CellGroups> cells)
         {
-            public int Mask { get; }
-            public int Discriminator { get; }
-            public string Description { get; }
-            public IGrouping<int, CellGroups> Cells { get; }
+            Mask = mask;
+            Discriminator = discriminator;
+            Description = description;
+            Cells = cells;
+        }
 
-            public TwoDigitMaskGroups(int mask, int discriminator, string description, IGrouping<int, CellGroups> cells)
+        public override string ToString()
+        {
+            var result = $"{nameof(Mask)}: {Mask}, {nameof(Discriminator)}: {Discriminator}, {nameof(Description)}: {Description}\n";
+            foreach (var cellGroups in Cells)
             {
-                Mask = mask;
-                Discriminator = discriminator;
-                Description = description;
-                Cells = cells;
+                result += $"   {cellGroups.ToString()}\n";
             }
 
-            public override string ToString()
-            {
-                var result = $"{nameof(Mask)}: {Mask}, {nameof(Discriminator)}: {Discriminator}, {nameof(Description)}: {Description}\n";
-                foreach (var cellGroups in Cells)
-                {
-                    result += $"   {cellGroups.ToString()}\n";
-                }
-
-                return result;
-            }
+            return result;
         }
     }
 }
