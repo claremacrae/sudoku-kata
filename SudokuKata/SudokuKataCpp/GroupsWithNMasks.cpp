40a83a5e9252b546dc112770007607e5ced9021f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-06-05 11:42:24 +0100

    F Print out more detail in GroupsWithNMasks in C++

--- SudokuKata/SudokuKataCpp/GroupsWithNMasks.cpp
+++ SudokuKata/SudokuKataCpp/GroupsWithNMasks.cpp
@@ -20,12 +20,32 @@ std::wstring GroupsWithNMasks::ToString() const
 {
     std::wstring result = fmt::format(
         L"{{ Mask = {0}, Description = {1}, Cells = {2}, "
-        L"CellsWithMask = {3}, CleanableCellsCount = {4} }}",
+        L"CellsWithMask = {3}, CleanableCellsCount = {4}",
         Mask,
         Description,
         Cells.size(),
         CellsWithMask.size(),
         CleanableCellsCount
         );
+    result += L"\n   Cells:";
+    //                                     std::wstring valuesReport = fmt::format(L"{}", fmt::join(valuesToRemove, L", "));
+
+//    result += string.Join("\n      ", Cells);
+//    result += fmt::format(L"{}", fmt::join(Cells, L"\n      "));
+    for (const auto& item : Cells)
+    {
+        result += L"\n      ";
+        result += item.ToString();
+    }
+    result += L"\n   CellsWithMask:";
+//    result += fmt::format(L"{}", fmt::join(CellsWithMask, L"\n      "));
+    for (const auto& item : CellsWithMask)
+    {
+        result += L"\n      ";
+        result += item.ToString();
+    }
+
+    //    result += string.Join("\n      ", CellsWithMask);
+    result += L"\n}";
     return result;
 }
