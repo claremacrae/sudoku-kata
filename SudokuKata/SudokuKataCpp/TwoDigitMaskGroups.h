9c73fa4f523946f564acda53fcd0db8d104b305f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-06-05 21:40:28 +0100

    r Remove #include of unused header stringhelper.h

--- SudokuKata/SudokuKataCpp/TwoDigitMaskGroups.h
+++ SudokuKata/SudokuKataCpp/TwoDigitMaskGroups.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <string>
-#include "stringhelper.h"
 #include "CellGroups.h"
 
 #include <vector>
--- SudokuKata/SudokuKataCpp/Utilities/CandidatesStringifier.h
+++ SudokuKata/SudokuKataCpp/Utilities/CandidatesStringifier.h
@@ -2,7 +2,6 @@
 
 #include <string>
 #include <vector>
-#include "../stringhelper.h"
 
 namespace SudokuKata::Utilities
 {
