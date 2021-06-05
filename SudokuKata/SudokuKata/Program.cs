c55f4c5f2d19583822e98e0aac265e50726738f7
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-06-05 12:50:27 +0100

    *** Re-enable last step of C# solution code - read for C++ port

--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -666,7 +666,7 @@ namespace SudokuKata
                 }
 
                 #region Final attempt - look if the board has multiple solutions
-                if (false && !changeMade) // TODO ** UNDO
+                if (!changeMade)
                 {
                     // This is the last chance to do something in this iteration:
                     // If this attempt fails, board will not be entirely solved.
